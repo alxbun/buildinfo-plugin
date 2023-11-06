14595b0e96494f99b0f1a75431729c1b22ea7956
Alxbun <>
2023-11-06 17:03:57 +0300

    1.0.1 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.github.alxbun"
 description = "Gradle Buildinfo Plugin"
-version = "1.0.0"
+version = "1.0.1"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
